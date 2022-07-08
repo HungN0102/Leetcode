aba3d25a85d99f44a774d0ef05338203678ffc0c
HungN0102 <b.hungng21@gmail.com>
2022-07-08 13:29:06 +0100

    FIX 238

--- scripts/Product of Array Except Self 238.py
+++ scripts/Product of Array Except Self 238.py
@@ -1,13 +1,26 @@
 def arrayOfProducts(array):
+    fromLeftProducts = [1 for _ in range(len(array))]
+    fromRightProducts = [1 for _ in range(len(array))]
+    
     outputList = []
-    leftArea = 1
+    product = 1
     for i in range(len(array)):
-        number = leftArea
-        leftIndex = i + 1
-        while leftIndex <= len(array) - 1:
-            number *= array[leftIndex]
-            leftIndex += 1
-        
-        leftArea *= array[i]
-        outputList.append(number)
+        fromLeftProducts[i] = product*array[i]
+        product *= array[i]
+    
+    product = 1
+    for i in reversed(range(len(array))):
+        fromRightProducts[i] = product*array[i]
+        product *= array[i]
+    
+    for i in range(len(array)):
+        product = 1
+        if i < len(array) - 1:
+            product *= fromRightProducts[i+1]
+        if i > 0:
+            product *= fromLeftProducts[i-1]
+        outputList.append(product)
     return outputList
+
+
+arrayOfProducts([1,2,3,4])
