5031594ea89671cffcfc50bd21800caf6882517e
HungN0102 <b.hungng21@gmail.com>
2022-07-29 15:39:19 +0100

    O(N) O(N)

--- problems/Sliding Windows/Permutation in a string.py
+++ problems/Sliding Windows/Permutation in a string.py
@@ -1,7 +1,6 @@
 # Given a string and a pattern, find out if the string contains any permutation of the pattern.
 
 # Permutation is defined as the re-arranging of the characters of the string. For example, “abc” has the following six permutations:
-
 def find_permutation(string, pattern):
     hashmap = {}
     uniqueLetter = 0
