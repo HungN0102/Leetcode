02529f9e6c70c37092ca060c583a41bbaffe8fb6
HungN0102 <b.hungng21@gmail.com>
2022-08-06 21:51:59 +0100

    O(N) O(N)

---
+++ problems/Merge Intervals/253. Meeting Rooms II.py
@@ -0,0 +1,14 @@
+from heapq import *
+
+class Solution(object):
+    def minMeetingRooms(self, intervals):
+        intervals.sort(key=lambda x: x[0])
+        heap = []
+        maxRooms = 0
+        
+        for i in range(len(intervals)):
+            while len(heap) > 0 and intervals[i][0] >= heap[0]:
+                heappop(heap)
+            heappush(heap,intervals[i][1])
+            maxRooms = max(maxRooms, len(heap))
+        return maxRooms
