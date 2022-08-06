c7b8095fb78bab6af004dd60ada8ea9e75204b0e
HungN0102 <b.hungng21@gmail.com>
2022-08-06 22:08:06 +0100

    O(NlogN)

--- problems/Merge Intervals/Maximum CPU Load.py
+++ problems/Merge Intervals/Maximum CPU Load.py
@@ -1,3 +1,5 @@
+# We are given a list of Jobs. Each job has a Start time, an End time, and a CPU load when it is running. Our goal is to find the maximum CPU load at any time if all the jobs are running on the same machine.
+
 from heapq import *
 
 intervals = [[1,4,2], [2,4,1], [3,6,5]]
