76b355b6dd9de3fad721363c8a9a6fe92643b7c1
HungN0102 <b.hungng21@gmail.com>
2022-08-18 12:01:51 +0100

    O(N*2**N) O(N*2**N)

---
+++ problems/Subsets/Find all distinct subsets.py
@@ -0,0 +1,14 @@
+# Given a set with distinct elements, find all of its distinct subsets.
+nums = [1, 5, 3]
+def solution(nums):
+    subsets = []
+    subsets.append([])
+    
+    for currentNum in nums:
+        n = len(subsets)
+        for i in range(n):
+            setCopy = list(subsets[i])
+            setCopy.append(currentNum)
+            subsets.append(setCopy)
+            
+    return subsets
