cf78137ac73bb5b348474399e25e36773a068544
HungN0102 <b.hungng21@gmail.com>
2022-09-30 12:14:39 +0100

    O(N*M)

---
+++ problems/Famous Algorithms/12. Integer to Roman.py
@@ -0,0 +1,32 @@
+from re import M
+
+
+def intToRoman(number):
+    hashmap = {
+        1000:"M",
+        900:"CM",
+        500:"D",
+        400:"CD",
+        100:"C",
+        90:"XC",
+        50:"L",
+        40:"XL",
+        10:"X",
+        9:"IX",
+        5:"V",
+        4:"IV",
+        1:"I" 
+    }
+    
+    stringOutput = ""
+    while number != 0:
+        for k in hashmap:
+            floorResult = number // k
+            if floorResult >= 1:
+                stringOutput += floorResult*hashmap[k]
+                number -= floorResult*k
+                break 
+            
+    return stringOutput
+
+intToRoman(58)
