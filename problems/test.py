f9dc31772353f9fe43d528f42cfdbc1957aba540
HungN0102 <b.hungng21@gmail.com>
2022-10-09 22:52:15 +0100

    O(NM)

--- problems/test.py
+++ problems/test.py
@@ -1,28 +1,19 @@
-array = [1,2,3,4]
+array = [1,2,7,1,5]
+target = 9
 
-def DP(array):
-    total = sum(array)
-    if total % 2 != 0 or len(array) <= 1:
-        return False 
-    
-    target = int(total/2)
+def DP(array, target):
     results = [[0 for _ in range(target+1)] for _ in range(len(array))]
     
-    # for any subset, it can make 0
     for index in range(len(array)):
-        results[index][0] = True 
+        results[index][0] = 1
         
-    # for the first value, it can make itself or 0
     for t in range(target+1):
-        results[0][t] = False
-        if t == 0 or t == array[0]:
-            results[0][t] = True 
-    
-    for index in range(1,len(array)):
+        results[0][t] = 0
+        if t == array[0] or t == 0:
+            results[0][t] = 1
+            
+    for index in range(len(array)):
         for t in range(target+1):
-            results[index][t] = results[index-1][t]
-            if not results[index][t] and t-array[index] >= 0:
-                results[index][t] = results[index-1][t-array[index]]
-    
+            results[index][t] = results[index-1][t] + results[index-1][t-array[index]]
     return results[-1][-1]
-DP(array)
+DP(array, target)
