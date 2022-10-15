8b50c9b6777c4dadf6339c8c7617b2c0418c77f6
HungN0102 <b.hungng21@gmail.com>
2022-10-15 17:08:21 +0100

    O(N)

--- Algorithms/problems/test.py
+++ Algorithms/problems/test.py
@@ -1,30 +1,12 @@
-def diskStacking(disks):
-    stacks = [[] for _ in range(len(disks))]
-    maxHeights = [0 for _ in range(len(disks))]
+def coinChange(coins, amount):
+    coins.sort()
+    dp = [float("inf") for _ in range(amount+1)]
+    dp[0] = 0
     
-    stacks[0] = disks[0]
-    maxHeights[0] = disks[0][-1]
-    for currentIndex in range(len(disks)):
-        for secondIndex in range(0, currentIndex):
-            if checkStack(disks[currentIndex], disks[secondIndex]) and maxHeights[currentIndex] > disks[secondIndex][-1] + disks[currentIndex][-1]:
-                stacks[currentIndex] = stacks[secondIndex] + disks[currentIndex]
-                maxHeights[currentIndex] = disks[secondIndex][-1] + disks[currentIndex][-1]
-    return maxHeights
-
-def checkStack(biggerDisk, smallerDisk):
-    for i in range(3):
-        if smallerDisk[i] > biggerDisk[i]:
-            return False 
-    return True
-
-
-disks = [
-    [2, 1, 2],
-    [3, 2, 3],
-    [2, 2, 8],
-    [2, 3, 4],
-    [1, 3, 1],
-    [4, 4, 5]
-  ]
-
-diskStacking(disks)
+    for target in range(1, amount+1):
+        for c in coins:
+            if target - c >= 0:
+                dp[target] = min(dp[target], dp[target-c]+1)
+        
+    return dp[-1]
+coinChange([186,419,83,408],6249)
