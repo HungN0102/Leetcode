d02ae9a550233a1e08df7703d43230a64de02ef2
HungN0102 <b.hungng21@gmail.com>
2022-10-17 23:27:33 +0100

    added

--- Algorithms/problems/test.py
+++ Algorithms/problems/test.py
@@ -1,12 +1,12 @@
-def coinChange(coins, amount):
-    coins.sort()
-    dp = [float("inf") for _ in range(amount+1)]
-    dp[0] = 0
-    
-    for target in range(1, amount+1):
-        for c in coins:
-            if target - c >= 0:
-                dp[target] = min(dp[target], dp[target-c]+1)
+    def canJump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        dp = [False for _ in range(len(nums))]
+        dp[0] = True
         
-    return dp[-1]
-coinChange([186,419,83,408],6249)
+        for index in range(len(nums)):
+            if index == True:
+                for addMe in range(1, nums[index]):
+                    dp[index+addMe] = True
