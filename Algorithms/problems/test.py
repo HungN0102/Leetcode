e9641778b18a1e5e3e85aca292ae85cd236f90d1
HungN0102 <b.hungng21@gmail.com>
2022-10-27 19:12:56 +0100

    added

--- Algorithms/problems/test.py
+++ Algorithms/problems/test.py
@@ -1,16 +1,17 @@
-def climbStairs(n):
-    return solveDP(n, {})
+def decodeWays(number):
+    return solveDP(str(number), 0)
 
-def solveDP(n, memo):
-    if n in memo:
-        return memo[n]
-    if n < 0:
-        return 0
-    if n == 0:
+def solveDP(number, index):
+    if index == len(number):
         return 1
+    if number[index] == "0":
+        return 0
 
-    memo[n] = solveDP(n-1, memo) + solveDP(n-2, memo)
-    
-    return memo[n]
 
-climbStairs(8)
+    result = solveDP(number, index+1)
+    if int(number[index:index+2]) <= 26 and index + 2 <= len(number):
+        result += solveDP(number, index+2)
+
+    return result
+
+decodeWays(111111111111111111111111111111111111111111111)
