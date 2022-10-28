1e8a17878f72fd6f982aeaa5d8c410fa33ba48ab
HungN0102 <b.hungng21@gmail.com>
2022-10-28 23:54:44 +0100

    added

--- Algorithms/problems/test.py
+++ Algorithms/problems/test.py
@@ -1,17 +1,25 @@
-def decodeWays(number):
-    return solveDP(str(number), 0)
+# def decodeWays(number):
+#     return solveDP(str(number), 0)
 
-def solveDP(number, index):
-    if index == len(number):
-        return 1
-    if number[index] == "0":
-        return 0
+# def solveDP(number, index):
+#     if index == len(number):
+#         return 1
+#     if number[index] == "0":
+#         return 0
 
 
-    result = solveDP(number, index+1)
-    if int(number[index:index+2]) <= 26 and index + 2 <= len(number):
-        result += solveDP(number, index+2)
+#     result = solveDP(number, index+1)
+#     if int(number[index:index+2]) <= 26 and index + 2 <= len(number):
+#         result += solveDP(number, index+2)
 
-    return result
+#     return result
 
-decodeWays(111111111111111111111111111111111111111111111)
+# decodeWays(111111111111111111111111111111111111111111111)
+
+
+A = 1
+B = 2
+
+C = A
+A = B
+B = C
