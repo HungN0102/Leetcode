38bdb67e226954f1441a83c92b031cece1297dc2
HungN0102 <b.hungng21@gmail.com>
2022-10-29 19:49:04 +0100

    added

--- Algorithms/problems/test.py
+++ Algorithms/problems/test.py
@@ -1,25 +1,17 @@
-# def decodeWays(number):
-#     return solveDP(str(number), 0)
+s = "A man, a plan, a canal: Panama1"
+def validP(s):
+    alphabets = [character.lower() for character in s if character.isalnum()]
+    s = "".join(alphabets)
 
-# def solveDP(number, index):
-#     if index == len(number):
-#         return 1
-#     if number[index] == "0":
-#         return 0
+    left = 0
+    right = len(s) - 1
+    
+    while left <= right:
+        if s[left] != s[right]:
+            return False
+        left += 1
+        right -= 1
+        
+    return True 
 
-
-#     result = solveDP(number, index+1)
-#     if int(number[index:index+2]) <= 26 and index + 2 <= len(number):
-#         result += solveDP(number, index+2)
-
-#     return result
-
-# decodeWays(111111111111111111111111111111111111111111111)
-
-
-A = 1
-B = 2
-
-C = A
-A = B
-B = C
+validP(s)
